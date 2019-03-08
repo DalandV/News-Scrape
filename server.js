1d7f37adc977edd33d7c8e542411a0ff1c2b42df
Daland Dixon <dalanddixon@Dalands-MBP.fios-router.home>
2019-03-08 17:24:39 -0500

    useNewUrlParser

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsdb";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 require("./routes/htmlRoutes")(app);
